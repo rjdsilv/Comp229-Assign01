537270fb4a96ef0b0fbc2158f7fe81e0546d6dad
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-09-27 08:21:41 -0400

    Removed the navigation bar. Going to use different approach

--- Comp229-Assign01/Comp229_Assign01.Master.designer.cs
+++ Comp229-Assign01/Comp229_Assign01.Master.designer.cs
@@ -29,14 +29,5 @@ namespace Comp229_Assign01 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm frmPorifolioSite;
-        
-        /// <summary>
-        /// bodyContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder bodyContent;
     }
 }
