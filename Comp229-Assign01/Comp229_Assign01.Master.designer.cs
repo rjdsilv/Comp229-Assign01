9764abc2d02cd5b6caf183025b045739b84f98b2
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-09-27 08:25:14 -0400

    Fixxed the error due to the lack of body place holder

--- Comp229-Assign01/Comp229_Assign01.Master.designer.cs
+++ Comp229-Assign01/Comp229_Assign01.Master.designer.cs
@@ -29,5 +29,14 @@ namespace Comp229_Assign01 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm frmPorifolioSite;
+        
+        /// <summary>
+        /// bodyContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder bodyContent;
     }
 }
